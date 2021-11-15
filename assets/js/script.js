53d6b639960a111a851fd8dd72a7e8f6cd50d264
Kerry Eames <Kerryeames@live.com>
2021-11-14 21:34:37 -0700

    I've added the functionality of applying background colors to the timeblocks dynamically, but I can't figure out how to directly compare the current time to the listed time

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@ currentDate.textContent = today.format("MMM Do, YYYY");
 //I need the timeblock containers to be the correct design
 //I need there to be 16 timeblock containers for each hour of the waking day
 //I need to apply the existing CSS to the containers
+
+//The timeblock need to be color coded for the time of day in past/present/future by the hour
+
+//I need to specify the current hour. I can use the today var to do this
+//I need to create a target var for the timeblocks
+
+var currentHour = document.querySelector(".hour");
+var calendarPlans = document.querySelectorAll("#plans");
+
+if (currentHour.textContent !== today.format("ha")) {
+    for (var i = 0; i < calendarPlans.length; i++)
+    calendarPlans[i].setAttribute("class", "past col-12 col-xl-8");
+}
+
+console.log(currentHour.textContent)
